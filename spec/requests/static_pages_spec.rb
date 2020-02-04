87e0938d2f500fff263b643205419ccc86df4657
kohei <3173010@stu.hus.ac.jp>
2020-02-04 13:16:57 +0900

    add integration_test for static_page

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+include ApplicationHelper
+
+RSpec.describe "StaticPages", type: :request do
+  describe "GET /" do
+    before do
+      get root_path
+    end
+
+    it "success" do
+      expect(response).to have_http_status(200)
+      expect(response).to be_success
+      assert_select "title", full_title("Home")
+    end
+  end
+end
