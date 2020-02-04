e315426c924caa1d40837aab3021b3ff3538c631
kohei <3173010@stu.hus.ac.jp>
2020-02-04 16:13:53 +0900

    refoctoring features/static_page_spec.rb

--- spec/features/static_page_spec.rb
+++ spec/features/static_page_spec.rb
@@ -9,30 +9,10 @@ RSpec.feature "StaticPages", type: :feature do
     end
 
     describe "title" do
-      it "Gathers : Homeになること" do
-        expect(page).to have_title(full_title("Home"))
-      end
+      it { expect(page).to have_title(full_title("Home"))}
     end
     describe "body" do
-      describe "画面の表示" do
-        describe "header" do
-          it { expect(page).to have_content "新規ユーザー"}
-          it { expect(page).to have_content "ログイン" }
-          it { expect(page).to have_content "ユーザー"}
-          it { expect(page).to have_content "マイアカウント"}
-          it { expect(page).to have_content "プロフィール"}
-          it { expect(page).to have_content "ログアウト"}
-        end
-
-        describe "contents" do
-          it { expect(page).to have_css("h1", text: "発想を発信しよう") }
-        end
-
-        describe "footer" do
-          it {expect(page).to have_content "2020 Gathers by k.suzuki"}
-        end
-      end
-
+      it { expect(page).to have_css("h1", text: "発想を発信しよう") }
     end
   end
 
