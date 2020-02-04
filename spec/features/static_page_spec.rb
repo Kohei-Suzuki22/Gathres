a661157c42a96948895fbd085e5272bd4aa70192
kohei <3173010@stu.hus.ac.jp>
2020-02-04 13:36:02 +0900

    add footer_test

---
+++ spec/features/static_page_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+include ApplicationHelper
+
+RSpec.feature "StaticPages", type: :feature do
+
+  describe "#home" do
+    before do
+      visit root_path
+    end
+
+    describe "title" do
+      it "Gathers : Homeになること" do
+        expect(page).to have_title(full_title("Home"))
+      end
+    end
+    describe "body" do
+      it { expect(page).to have_css("h1", text: "発想を発信しよう") }
+    end
+  end
+
+end
