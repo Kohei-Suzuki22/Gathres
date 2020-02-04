0ccbea9e57745c3301b9405affb413e02967de0c
kohei <3173010@stu.hus.ac.jp>
2020-02-05 03:08:21 +0900

    add link_to signup_path

--- spec/requests/header_spec.rb
+++ spec/requests/header_spec.rb
@@ -6,10 +6,10 @@ RSpec.describe "Headers", type: :request do
       get root_path
       assert_template "static_pages/home"
       assert_select "a[href=?]", root_path, count: 2
+      assert_select "a[href=?]", signup_path
 
       pending "routeing作成次第"
       assert_select "a[href=?]", login_path
-      assert_select "a[href=?]", signup_path
       assert_select "a[href=?]", users_path
       assert_select "a[href=?]", user_path
       assert_select "a[href=?]", logout_path
